3ad000b4de27db6f42e16511e7dc28fff5d1edb6
Benjamim Lacerda <benjamim.lacerda16@gmail.com>
2024-07-30 19:00:22 -0300

    Criacao do app e config api

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { md5 } from 'js-md5';
+
+const timestamp = new Date().getTime();
+const privateKey = '87fa489e3b6ede3be6a73bfddd29ac30ac6314d1';
+const publicKey = 'e73c0da38e17f3161b7218b2fe621608';
+
+const hash = md5(timestamp + privateKey + publicKey);
+
+export const api = axios.create({
+    baseURL: 'https://gateway.marvel.com/v1/public/',
+    params: {
+        apikey: 'e73c0da38e17f3161b7218b2fe621608',
+        hash,
+        ts: timestamp,
+    },
+});
